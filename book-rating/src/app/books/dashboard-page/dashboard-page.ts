e6f71936de6d2a767a6725a8e78dc5732859a841
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-17 11:23:35 +0200

    Signal books

--- book-rating/src/app/books/dashboard-page/dashboard-page.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
-export class DashboardPage {}
+export class DashboardPage {
+  protected readonly books = signal<Book[]>([]);
+
+  constructor() {
+    this.books.set([
+      {
+        isbn: '456',
+        title: 'Angular',
+        description: 'Das Praxisbuch',
+        rating: 5,
+        price: 39.9,
+        authors: ['Ferdinand Malcher', 'Danny Koppenhagen', 'Johannes Hoppe']
+      },
+      {
+        isbn: '789',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        rating: 3,
+        price: 36.9,
+        authors: ['Fabian Deitelhoff']
+      }
+    ]);
+  }
+}
