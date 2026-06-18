c628ae0c075db11ae3168e6e1d728cd19497ea08
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-18 12:03:42 +0200

    Test Dashboard: Service durch Stub ersetzen

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPage } from './dashboard-page';
+import { BookRatingHelper } from '../shared/book-rating-helper';
+import { Book } from '../shared/book';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
@@ -9,6 +11,17 @@ describe('DashboardPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DashboardPage],
+      providers: [
+        // BRH ersetzen: Immer wenn jemand den Service anfordert,
+        // wird stattdessen das Ersatzobjekt ausgeliefert.
+        {
+          provide: BookRatingHelper,
+          useValue: {
+            rateUp: (book: Book) => book,
+            rateDown: (book: Book) => book,
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardPage);
