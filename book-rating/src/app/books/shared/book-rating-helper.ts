68721885be9193e15999b528551b2d32d0b905ab
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-18 10:52:39 +0200

    Implementierung BookRatingHelper, Immutability

--- book-rating/src/app/books/shared/book-rating-helper.ts
+++ book-rating/src/app/books/shared/book-rating-helper.ts
@@ -1,14 +1,24 @@
 import { Service } from '@angular/core';
 import { Book } from './book';
 
-@Service() // ab Angular 22
 // bis Angular 21: @Injectable({ providedIn: 'root' })
+@Service() // ab Angular 22
 export class BookRatingHelper {
     rateUp(book: Book): Book {
-        return book; // TODO!
+        if (book.rating >= 5) {
+            return book;
+        }
+        
+        return {
+            ...book,
+            rating: book.rating + 1
+        };
     }
 
     rateDown(book: Book): Book {
-        return book; // TODO!
+        return {
+            ...book,
+            rating: Math.max(1, book.rating - 1)
+        };
     }
 }
