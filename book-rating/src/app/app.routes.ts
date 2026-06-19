8082441e82fa894b5708f76c259e3cd74b98d6a8
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-19 11:57:07 +0200

    Routing: Redirect und Wildcard

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { booksRoutes } from './books/books.routes';
+import { ErrorPage } from './error-page/error-page';
 
 export const routes: Routes = [
-    ...booksRoutes
+    // bei Weiterleitung vom leeren Pfad: fast immer pathMatch:full nötig
+    { path: '', redirectTo: 'books', pathMatch: 'full' },
+    ...booksRoutes,
+    // Wildcard-Route: muss immer ganz unten stehen!
+    { path: '**', component: ErrorPage }
 ];
