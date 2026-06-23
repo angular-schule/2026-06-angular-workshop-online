2d54debff866c1a3989236e2797827faa09df44b
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-23 10:34:05 +0200

    RxJS: Observables erstellen

--- rxjs-playground/src/app/exercise-creating/exercise-creating.ts
+++ rxjs-playground/src/app/exercise-creating/exercise-creating.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Subscriber, Observer } from 'rxjs';
 
 import { HistoryWindow } from '../shared/history-window/history-window';
 
@@ -23,6 +23,57 @@ export class ExerciseCreating {
      */
 
     /******************************/
+
+    function myOf<T>(...values: T[]): Observable<T> {
+      return new Observable<T>(sub => {
+        values.forEach(v => sub.next(v));
+        sub.complete();
+      });
+    }
+
+    // interval(1000)         // ---0---1---2---3---4---5---6 ...
+    // timer(3000)            // ---------0|
+    // timer(3000, 1000)      // ---------0---1---2---3---4---5---6 ...
+    // timer(0, 1000)         // 0---1---2---3---4---5---6 ...
+
+    timer(0, 1000).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE'),
+    });
+
+
+
+
+    /******************************/
+
+    // PRODUCER: generiert Werte
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+      sub.next(5);
+      sub.next(10);
+      sub.complete();
+      sub.next(15);
+      // fetch('/api').then(res => res.json()).then(data => sub.next(data))
+
+      setTimeout(() => sub.next(100), 2000);
+      setTimeout(() => sub.next(500), 4000);
+      setTimeout(() => sub.complete(), 6000);
+    }
+
+    // OBSERVER: verarbeitet die Werte
+    const obs: Observer<number> = {
+      next: (e) => console.log(e),
+      error: (error: any) => console.error(error),
+      complete: () => console.log('FERTIG')
+    };
+
+    // producer(obs);
+    // OBSERVABLE: Schnittstelle zwischen PRODUCER und OBSERVER
+    // vermittelt die Werte zwischen Quelle und Ziel
+    // Finnische Notation $
+    const myObs$ = new Observable(producer);
+    // myObs$.subscribe(obs);
 
     
     /******************************/
