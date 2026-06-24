74d704946555c9924a2cfa7ce57b403e022b4400
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-24 10:58:13 +0200

    Suche: Suchformular mit Signal Forms

--- book-rating/src/app/books/book-search-page/book-search-page.ts
+++ book-rating/src/app/books/book-search-page/book-search-page.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-book-search-page',
-  imports: [],
+  imports: [FormField],
   templateUrl: './book-search-page.html',
   styleUrl: './book-search-page.scss',
 })
-export class BookSearchPage {}
+export class BookSearchPage {
+  protected readonly searchTerm = signal('');
+  protected readonly searchForm = form(this.searchTerm);
+
+  constructor() {
+    toObservable(this.searchTerm).subscribe(e => console.log(e));
+  }
+}
