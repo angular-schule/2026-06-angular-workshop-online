58e13d300946bce08e824efec3ee066084c640b0
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-24 15:22:18 +0200

    Lösung Zusatzaufgabe: Login-Bereich

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -2,14 +2,19 @@ import { Routes } from '@angular/router';
 // import { booksRoutes } from './books/books.routes';
 import { ErrorPage } from './error-page/error-page';
 import { HomePage } from './home-page/home-page';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     // bei Weiterleitung vom leeren Pfad: fast immer pathMatch:full nötig
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePage },
-    
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes)
+    },
     {
         path: 'books',
+        canActivate: [authGuard],
         loadChildren: () => import('./books/books.routes').then(m => m.booksRoutes)
     },
 
---
+++ book-rating/src/app/auth/auth.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { LoginPage } from './login-page/login-page';
+
+export const authRoutes: Routes = [
+  { path: 'login', component: LoginPage },
+];
