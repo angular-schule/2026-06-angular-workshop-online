f742beefaeea6281fcbe666dde0ba1648da4fe87
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-24 11:38:49 +0200

    Suche mit Resource und debounced()

--- book-rating/src/app/books/book-search-page/book-search-page.ts
+++ book-rating/src/app/books/book-search-page/book-search-page.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, debounced, inject, signal } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { form, FormField } from '@angular/forms/signals';
+import { BookStore } from '../shared/book-store';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-search-page',
-  imports: [FormField],
+  imports: [FormField, JsonPipe],
   templateUrl: './book-search-page.html',
   styleUrl: './book-search-page.scss',
 })
@@ -12,7 +14,8 @@ export class BookSearchPage {
   protected readonly searchTerm = signal('');
   protected readonly searchForm = form(this.searchTerm);
 
-  constructor() {
-    toObservable(this.searchTerm).subscribe(e => console.log(e));
-  }
+  protected readonly debouncedSearchTerm = debounced(this.searchTerm, 1000);
+  protected readonly searchTermMin3 = computed(() => this.debouncedSearchTerm.value().length >= 3 ? this.debouncedSearchTerm.value() : undefined);
+
+  protected readonly results = inject(BookStore).searchResource(this.searchTermMin3);
 }
